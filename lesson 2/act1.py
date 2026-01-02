4584aabbb0da2ddd18418602ac456c96bb8b60f5
muawinmuzami <muzamilnazmi911@gmail.com>
2026-01-02 18:27:42 +0530

    my 1

---
+++ lesson 2/act1.py
@@ -0,0 +1,10 @@
+#this is my coding world
+print("welcome to the codingal world")
+#new year
+print(2026)
+#new line 
+print("i am muawin\n")
+#mult arguments together
+print(1,"muawin")
+#in arg
+print("muawin",end="1234")
