0d766b492da3e46aff29dfe285c0b992f92f134f
muawinmuzami <muzamilnazmi911@gmail.com>
2026-01-05 18:29:29 +0530

    act.1

---
+++ lesson 3/act1.py
@@ -0,0 +1,8 @@
+age=12
+height=5.4
+isstudent=True
+name="muawin"
+print("type of age:",type(age))
+print("type of :height",type(height))
+print("type of :isstudent",type(isstudent))
+print("type of :name",type(name))
