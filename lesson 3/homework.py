aa4e062259008c0aeaeb106d41f0f784d175a421
muawinmuzami <muzamilnazmi911@gmail.com>
2026-01-07 18:42:33 +0530

    h/w.py

---
+++ lesson 3/homework.py
@@ -0,0 +1,16 @@
+# creating different variables
+faheem = "3 may 2012"
+rubaan = "5 jan 2013"
+taha = "5 may 2012"
+mueed = "8 aug 2012"
+
+print(
+"these are my friend's birthday dates :"
+"\nfaheem, rubaan, taha and mueed"
+)
+
+# printing the variables
+print(faheem)
+print(rubaan)
+print(taha)
+print(mueed)
