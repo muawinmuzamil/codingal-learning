4b8c041f2fbf6fe32b77256a9d2afa063994892d
muawinmuzami <muzamilnazmi911@gmail.com>
2026-01-08 18:23:49 +0530

    congrats22

---
+++ lesson 3/homework2.py
@@ -0,0 +1,10 @@
+# String operations for a congratulation message
+
+name = "Faheem"
+event = "winning the competition"
+
+message = "Congratulations " + name.upper() + "!" \
+          "\nYou have achieved success in " + event.title() + "." \
+          "\nWe are very proud of you."
+
+print(message)
