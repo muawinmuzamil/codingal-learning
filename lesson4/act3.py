40c10a7067cec3f810bf459bab4acb720c007f48
muawinmuzami <muzamilnazmi911@gmail.com>
2026-01-09 18:46:31 +0530

    act3

---
+++ lesson4/act3.py
@@ -0,0 +1,8 @@
+math= int(input("math :"))
+english= int(input("english : "))
+science= int(input("science : "))
+urdu= int(input("urdu : "))
+# percentage
+sum=(math+science+english+urdu)
+perc=(sum/400)*100
+print(perc)
