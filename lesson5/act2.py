2141235a7ec148d5f0555864c088fe10fd48a033
muawinmuzami <muzamilnazmi911@gmail.com>
2026-01-12 18:37:46 +0530

    act2

---
+++ lesson5/act2.py
@@ -0,0 +1,11 @@
+actualamount = float(input("enter your amount"))
+sellingamount = float(input("enter your amount"))
+if sellingamount>actualamount:
+    print(sellingamount-actualamount)
+else :
+    print("loss")    
+
+ 
+
+
+    
