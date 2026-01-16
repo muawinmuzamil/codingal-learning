10b9401598977b15fd2242522acf6b9fc2d5f872
muawinmuzami <muzamilnazmi911@gmail.com>
2026-01-16 18:52:25 +0530

    act3

---
+++ lesson 6/act3.py
@@ -0,0 +1,11 @@
+height = float(input("enter your height in cm"))
+weight = float(input("enter your weight in kg"))
+bmi = weight / (height/100)**2
+if bmi <= (18.4):
+    print("under weight")
+elif bmi <= (24.9):
+    print("healthy")
+elif bmi <= (34.9):
+    print("you are over weight") 
+else:
+    print("obease")       
