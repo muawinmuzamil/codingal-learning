c42815f2b6f6c94df9d61fce76d557e32513603b
muawinmuzami <muzamilnazmi911@gmail.com>
2026-01-26 22:02:09 +0530

    hmw

--- homework.py
+++
@@ -1,7 +0,0 @@
-age = int(input("Enter your age: "))
-
-if age <20 :
-    print("you can enroll")
-
-else:
-    print("you are a minor")
---
+++ lesson9/homework.py
@@ -0,0 +1,8 @@
+age = int(input("Enter your age: "))
+
+if age <20:
+    print("valid age to enroll")
+
+
+else:
+    print("You are a minor")
