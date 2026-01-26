4e63b5cec84bfffc4d155054a1e157e45e1be9f4
muawinmuzami <muzamilnazmi911@gmail.com>
2026-01-26 18:20:31 +0530

    act1

---
+++ lesson9/act1.py
@@ -0,0 +1,11 @@
+medical_cause=input("did you have a medical cause Y or n:")
+atten=int(input("enter your attendence"))
+if medical_cause=="Y":
+ print("YOU ARE A LLOWED")
+else:
+ if atten>=75:
+  print("allowed")
+ else:
+  print("not allowed")
+ 
+
