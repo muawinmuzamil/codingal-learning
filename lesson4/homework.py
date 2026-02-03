2931aec52aa8f3be71f9e23ceb113014b3bcf3d3
muawinmuzami <muzamilnazmi911@gmail.com>
2026-02-03 11:01:38 +0530

    new

--- lesson4/homework.py
+++ lesson4/homework.py
@@ -1,5 +1,5 @@
 # The number for which you want to find the square root
 number = int(input("enter your number"))
-square_root = number ** number
+square_root = number ** 0.5
 
 print(f"The square root of {number} is {square_root}")
