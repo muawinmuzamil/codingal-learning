bac329c9e5227706f125bb43ea910df554af7298
muawinmuzami <muzamilnazmi911@gmail.com>
2026-02-21 10:12:13 +0530

    qwertyu

---
+++ lesson14/homework.py
@@ -0,0 +1,14 @@
+import turtle
+
+# Create a turtle object
+pen = turtle.Turtle()
+
+# Draw a square using while loop
+count = 0
+while count < 4:
+    pen.forward(100)   # Move forward by 100 units
+    pen.right(90)      # Turn right 90 degrees
+    count += 1
+
+# Finish
+turtle.done()
