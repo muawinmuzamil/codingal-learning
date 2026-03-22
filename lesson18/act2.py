64e17fb73fcb09f6cb7b0e085d2a2610d60818b3
muawinmuzami <muzamilnazmi911@gmail.com>
2026-03-22 17:58:16 +0530

    qwerty

---
+++ lesson18/act2.py
@@ -0,0 +1,19 @@
+try:
+    num1,num2=eval(input("enter 2 numbers , seperated by a comma:"))
+    result=num1/num2
+    print("result is ", result)
+
+
+except ZeroDivisionError:
+    print("Division by zero is error!!")
+
+except SyntaxError:
+    print("comma is missing , enter numbers seperated by comma like this 1,1,2")
+
+except:
+    print("wrong input")
+
+else:
+    print("no exceptions") 
+finally:
+    print("this will execute no matter what")       
